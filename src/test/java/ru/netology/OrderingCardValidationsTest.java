c3d91e244127e68060619b49a86cc037fa330fb3
Anton <smirnovanton123@icloud.com>
2023-03-29 22:12:51 +0300

    main

--- src/test/java/ru/netology/OrderingCardValidationsTest.java
+++ src/test/java/ru/netology/OrderingCardValidationsTest.java
@@ -21,7 +21,7 @@ public class OrderingCardValidationsTest {
     }
 
     @BeforeEach
-    static void setUpAll() {
+    void setUpAll() {
 // убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
@@ -109,7 +109,7 @@ public class OrderingCardValidationsTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78956231122");
         driver.findElement(By.className("button__text")).click();
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invaligid")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).getText().trim();
         assertEquals(expected, actual);
     }
 
